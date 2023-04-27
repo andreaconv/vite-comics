8f926467fd1cfd6f41b065b053a649cbf126b0d6
andreaconv <andrea2convertini@gmail.com>
2023-04-28 00:03:17 +0200

    end Brands

--- src/components/Brands.vue
+++ src/components/Brands.vue
@@ -1,9 +1,11 @@
 <script>
+import {brands} from '../data/Brands-menu.js'
+
 export default {
   name: 'Brands',
   data(){
     return{
-      title: 'Brands'
+      brands,
     }
   }
 }
@@ -15,7 +17,14 @@ export default {
 
     <div class="container">
   
-      <h1>{{title}}</h1>
+      <ul>
+        <li
+            v-for="(link, index) in brands"
+            :key="index">
+          <img :src="link.path">
+          <a href="#">{{link.text}}</a>
+        </li>
+      </ul>
   
     </div>
 
@@ -29,13 +38,43 @@ export default {
 
 @use '../scss/partials/generals' as *;
 @use '../scss/partials/vars' as *;
+@use '../scss/partials/mixin' as *;
+
 
 .brands{
   height: 120px;
   background-color: $primary-color;
-}
-  h1{
-    color: white;
+
+  .container{
+    @include centerFlex('vertical');
+    height: 100%;
+
+    ul{
+      display: flex;
+      justify-content: space-evenly;
+      height: 100%;
+      width: 100%;
+    }
+
+    li{
+      height: 100%;
+      @include centerFlex('vertical');
+
+      img{
+        height: 40%;
+        margin-right: 1rem;
+      }
+
+      a{
+        color: white;
+        text-transform: uppercase;
+      }
+
+      &:last-child img{
+          height: 30%;
+        }
+    }
   }
+}
 
 </style>
---
+++ src/data/Brands-menu.js
@@ -0,0 +1,29 @@
+const brands = [
+  {
+    link: '#',
+    path: './src/assets/img/buy-comics-digital-comics.png',
+    text: 'digital comics'
+  },
+  {
+    link: '#',
+    path: './src/assets/img/buy-comics-merchandise.png',
+    text: 'DC merchandise'
+  },
+  {
+    link: '#',
+    path: './src/assets/img/buy-comics-subscriptions.png',
+    text: 'subscription'
+  },
+  {
+    link: '#',
+    path: './src/assets/img/buy-comics-shop-locator.png',
+    text: 'comic shop locator'
+  },
+  {
+    link: '#',
+    path: './src/assets/img/buy-dc-power-visa.svg',
+    text: 'dc power visa'
+  },
+]
+
+export {brands}
